189be3d30acbb6c24dde006790c91745d4596bed
Unknown <matheusklauck8@gmail.com>
2019-01-18 14:12:09 -0200

    Authentication
    Criação das entidades de autenticação

---
+++ BaseExchange/Authentication/ApiCredentials.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseExchange.Authentication
+{
+    public class ApiCredentials
+    {
+        /// <summary>
+        /// The api key to authenticate requests
+        /// </summary>
+        public SecureString Key { get; }
+
+        /// <summary>
+        /// The api secret to authenticate requests
+        /// </summary>
+        public SecureString Secret { get; }
+
+        /// <summary>
+        /// The private key to authenticate requests
+        /// </summary>
+        public PrivateKey PrivateKey { get; }
+
+        /// <summary>
+        /// Create Api credentials providing a private key for authentication
+        /// </summary>
+        /// <param name="privateKey">The private key used for signing</param>
+        public ApiCredentials(PrivateKey privateKey)
+        {
+            PrivateKey = privateKey;
+        }
+
+        /// <summary>
+        /// Create Api credentials providing a api key and secret for authentication
+        /// </summary>
+        /// <param name="key">The api key used for identification</param>
+        /// <param name="secret">The api secret used for signing</param>
+        public ApiCredentials(SecureString key, SecureString secret)
+        {
+            Key = key;
+            Secret = secret;
+        }
+
+        /// <summary>
+        /// Create Api credentials providing a api key and secret for authentication
+        /// </summary>
+        /// <param name="key">The api key used for identification</param>
+        /// <param name="secret">The api secret used for signing</param>
+        public ApiCredentials(string key, string secret)
+        {
+            if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(secret))
+                throw new ArgumentException("Key and secret can't be null/empty");
+
+            var secureApiKey = new SecureString();
+            foreach (var c in key)
+                secureApiKey.AppendChar(c);
+            secureApiKey.MakeReadOnly();
+            Key = secureApiKey;
+
+            var secureApiSecret = new SecureString();
+            foreach (var c in secret)
+                secureApiSecret.AppendChar(c);
+            secureApiSecret.MakeReadOnly();
+            Secret = secureApiSecret;
+        }
+
+        public void Dispose()
+        {
+            Key?.Dispose();
+            Secret?.Dispose();
+            PrivateKey?.Dispose();
+        }
+    }
+}
---
+++ BaseExchange/Authentication/AuthenticationProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseExchange.Authentication
+{
+    public abstract class AuthenticationProvider
+    {
+        public ApiCredentials Credentials { get; }
+
+        protected AuthenticationProvider(ApiCredentials credentials)
+        {
+            Credentials = credentials;
+        }
+
+        public virtual Dictionary<string, object> AddAuthenticationToParameters(string uri, string method, Dictionary<string, object> parameters, bool signed)
+        {
+            return parameters;
+        }
+
+        public virtual Dictionary<string, string> AddAuthenticationToHeaders(string uri, string method, Dictionary<string, object> parameters, bool signed)
+        {
+            return new Dictionary<string, string>();
+        }
+
+        public virtual string Sign(string toSign)
+        {
+            return toSign;
+        }
+
+        public virtual byte[] Sign(byte[] toSign)
+        {
+            return toSign;
+        }
+
+        protected string ByteToString(byte[] buff)
+        {
+            var result = "";
+            foreach (var t in buff)
+                result += t.ToString("X2"); /* hex format */
+            return result;
+        }
+    }
+}
