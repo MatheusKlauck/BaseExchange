0f1cb46403f99f702a404f01c2c1006c2688590c
Unknown <matheusklauck8@gmail.com>
2019-01-18 15:04:14 -0200

    Added Sockets

---
+++ BaseExchange/Sockets/UpdateSubscription.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BaseExchange.Sockets
+{
+    public class UpdateSubscription
+    {
+        private readonly SocketSubscription subscription;
+
+        /// <summary>
+        /// Event when the connection is lost. The socket will automatically reconnect when possible.
+        /// </summary>
+        public event Action ConnectionLost
+        {
+            add => subscription.ConnectionLost += value;
+            remove => subscription.ConnectionLost -= value;
+        }
+
+        /// <summary>
+        /// Event when the connection is restored. Timespan parameter indicates the time the socket has been offline for before reconnecting
+        /// </summary>
+        public event Action<TimeSpan> ConnectionRestored
+        {
+            add => subscription.ConnectionRestored += value;
+            remove => subscription.ConnectionRestored -= value;
+        }
+
+        /// <summary>
+        /// The id of the socket
+        /// </summary>
+        public int Id => subscription.Socket.Id;
+
+        public UpdateSubscription(SocketSubscription sub)
+        {
+            subscription = sub;
+        }
+
+        /// <summary>
+        /// Close the subscription
+        /// </summary>
+        /// <returns></returns>
+        public async Task Close()
+        {
+            await subscription.Close().ConfigureAwait(false);
+        }
+    }
+}
---
+++ BaseExchange/Sockets/WebsocketFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using BaseExchange.Interfaces;
+using BaseExchange.Logging;
+
+namespace BaseExchange.Sockets
+{
+    public class WebsocketFactory : IWebsocketFactory
+    {
+        public IWebsocket CreateWebsocket(Log log, string url)
+        {
+            return new BaseSocket(log, url);
+        }
+
+        public IWebsocket CreateWebsocket(Log log, string url, IDictionary<string, string> cookies, IDictionary<string, string> headers)
+        {
+            return new BaseSocket(log, url, cookies, headers);
+        }
+    }
+}
