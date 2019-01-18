24f1e0808f3816d07172aaa527c3e0a6eef0415c
Unknown <matheusklauck8@gmail.com>
2019-01-18 14:54:10 -0200

    Added more converters

---
+++ BaseExchange/Converters/UTCDateTimeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BaseExchange.Converters
+{
+    public class UTCDateTimeConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(JsonConvert.SerializeObject(value));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value == null)
+                return null;
+
+            DateTime value;
+            if (reader.Value is string s)
+                value = (DateTime)JsonConvert.DeserializeObject(s);
+            else
+                value = (DateTime)reader.Value;
+
+            return DateTime.SpecifyKind(value, DateTimeKind.Utc);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime) || objectType == typeof(DateTime?);
+        }
+    }
+}
